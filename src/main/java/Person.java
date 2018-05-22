4f0bc847713caaac4e1ef1418013e36170cc3e7a
Sarah Bartlett <sarah.bartlett28@gmail.com>
2018-05-22 18:05:45 +0100

    person has name

--- src/main/java/Person.java
+++ src/main/java/Person.java
@@ -1,2 +1,18 @@
 public class Person {
+
+    String name;
+    int age;
+
+    public Person(String name, int age) {
+        this.name = name;
+        this.age = age;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getAge() {
+        return age;
+    }
 }
--- src/test/java/PersonTest.java
+++ src/test/java/PersonTest.java
@@ -1,2 +1,19 @@
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
 public class PersonTest {
+
+    Person person;
+
+    @Before
+    public void setUp() throws Exception {
+        person = new Person("Sarah", 30);
+    }
+
+    @Test
+    public void hasName(){
+        assertEquals("Sarah", person.getName());
+    }
 }
